87693615a8338111e33efdd02a25f9d337a51ab4
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-11-06 16:58:34 -0800

    exercises

--- child_pid.c
+++ child_pid.c
@@ -11,39 +11,35 @@
  */
 int main(void)
 {
-	int i;
-	pid_t child_pid;
-
-	for (i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		child_pid = fork();
+		pid_t child_pid = fork();
 
-		if (child_pid < 0)
+		if (child_pid == -1)
 		{
-			perror("Fork error");
-			exit(1);
+			perror("fork");
+			exit(EXIT_FAILURE);
 		}
 		else if (child_pid == 0)
 		{
-			execlp("ls", "ls", "/tmp", NULL);
+			printf("Child %d (PID: %d) running ls -l /tmp:\n", i + 1, getpid());
+			execl("/bin/ls", "ls", "-l", "/tmp", (char *)0);
 
-			perror("Exec error");
-			exit(1);
+			perror("execl");
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
 			int status;
 
 			waitpid(child_pid, &status, 0);
-
-			if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+			if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
 			{
-				printf("Child %d has exited successfully.\n", i + 1);
+				printf("Child %d (PID: %d) completed successfully.\n", i + 1, child_pid);
 			}
-
 			else
 			{
-				printf("Child %d has exited with an error.\n", i + 1);
+				printf("Child %d (PID: %d) encountered an error.\n", i + 1, child_pid);
 			}
 		}
 	}
