a48d6984f1bd4ca6cf6a7a7f685c952d12eed1fb
Omosalewa-Akinsete <shaleycoral@gmail.com>
2023-11-13 11:11:24 -0800

    simple_shell

---
+++ simple_shell_description.c
@@ -0,0 +1,13 @@
+#include <stdio.h> /* for printf */
+#include <unistd.h> /* for fork, execve */
+#include <stdlib.h> /* for standard library */
+#include <string.h> /* for strtok */
+#include <stddef.h> /* for standard definition */
+#include <errno.h> /* for errno, perror */
+#include <sys/types.h> /* for type pid */
+#include <sys/wait.h> /* for wait */
+#include <sys/stat.h> /* for use of stat function */
+#include <signal.h> /* for signal management */
+#include <fcntl.h> /* for open files */
+
+
